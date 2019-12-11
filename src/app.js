7abbfe0b1a4b14cff7ab26474991a71b4844b669
Goste64 <Aidanhamilton@live.com>
2019-12-11 11:18:36 +1000

    installed dependencies and began setup of express and mongodb

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+
+
+const port = 3000
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+const dbConn = 'mongodb://localhost/my_store'
+// Set four properties to avoid deprecation warnings:
+mongoose.connect(dbConn, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    },
+    (err) => {
+        if (err) {
+            console.log('Error connecting to database', err);
+        } else {
+            console.log('Connected to database!');
+        }
+    });
+
+app.listen(port, () => {
+	console.log(`REFCON group MERN app listening on port ${port}`)
+})
