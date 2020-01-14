aaa665a948ef57d0a91f57ab04955b8088b6db9f
Goste64 <Aidanhamilton@live.com>
2020-01-14 11:26:33 +1000

    added react scripts, started work on navbar component

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import react from 'react'
+import reactDOM from 'react-dom'
+
+class Nav extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            
+        }
+    }
+
+
+}
